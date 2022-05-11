9377ca1aefd246f3e3880343ed3c53f2880afc30
Tonainings <105057673+Tonainings@users.noreply.github.com>
2022-05-12 00:59:06 +0200

    fix identificationID con foto profilo 12/05
    fix per tipo documento e codice ID resi di sola lettura e aggiunta di immagine del profilo.

--- Views/profili/modifica.php
+++ Views/profili/modifica.php
@@ -37,9 +37,25 @@
 
 <main class="form-signin container text-center" style="display: flex !important; justify-content: center !important;">
 <form action="/profili/modify" method="post" enctype="multipart/form-data">
-<img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Modifica Profilo</h1>
 
+
+
+                                <?php
+                                if ($_SESSION['logged_in']) {
+                                echo "<div class=\"text-center\">";
+                                  echo " <h3 class=\"profile-username text-center\">".esc($title)."</h3>";
+                                echo "<img class=\"img-fluid rounded-circle\" src='" . $profilo['image'] . "' alt=\"Circle image\" height=\"200\" width=\"200\"/>";
+                                }
+                                ?>  
+
+
+
+
+
+
+<!-- <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+    
+    <h1 class="h3 mb-3 fw-normal">Modifica Profilo</h1>
     <div class="form-floating" style="width: 500px !important; border-bottom-style: solid !important; border-bottom-width: 0px !important; margin-bottom: 10px !important;">
     <?php
           if ($_SESSION['logged_in']) {
@@ -48,14 +64,24 @@
               
       <label for="userid">UserID</label>
     </div>
-    <select name="identificationid" for="identificationid" class="form-select form-select-sm" style="width: 500px !important; border-bottom-style: solid !important; border-bottom-width: 0px !important; margin-bottom: 10px !important;" aria-label=".form-select-sm example">
+    
+    <div class="form-floating" style="width: 500px !important; border-bottom-style: solid !important; border-bottom-width: 0px !important; margin-bottom: 10px !important;">
+    <?php
+          if ($_SESSION['logged_in']) {
+              echo "<input type=\"text\" readonly class=\"form-control\" name=\"identificationid\" value= \"".esc($profilo['identificationid'])."\" required>";
+              }?>
+              
+      <label for="userid">Documento</label>
+    </div>
+
+   <!-- <select name="identificationid" for="identificationid" class="form-select form-select-sm" style="width: 500px !important; border-bottom-style: solid !important; border-bottom-width: 0px !important; margin-bottom: 10px !important;" aria-label=".form-select-sm example">
     <option selected>Documento</option>
     <option name="identificationid" for="identificationid" value="patente">Patente</option>
-    <option name="identificationid" for="identificationid" value="carta_identità">Carta d'identità</option></select>
+    <option name="identificationid" for="identificationid" value="carta_identità">Carta d'identità</option></select>  -->
 
     <div class="form-floating" style="width: 500px !important; border-bottom-style: solid !important; border-bottom-width: 0px !important; margin-bottom: 10px !important;">
     <?php  if ($_SESSION['logged_in']) {  
-    echo "<input type=\"input\" class=\"form-control\" name=\"id_num\" value= \"".esc($profilo['id_num'])."\" required>";
+    echo "<input type=\"text\" readonly class=\"form-control\" name=\"id_num\" value= \"".esc($profilo['id_num'])."\" required>";
     }
        ?>
       <label for="id_num">Id_num</label>
